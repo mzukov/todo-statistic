6cedd904ccb1d240446069fa800b801321ab4e13
mzukov <mzukov@yandex.ru>
2025-03-04 15:52:27 +0500

    1, 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 const files = getFiles();
 
@@ -8,7 +8,10 @@ readLine(processCommand);
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+    return filePaths.map(path => ({
+        path: path,
+        content: readFile(path)
+    }));
 }
 
 function processCommand(command) {
@@ -16,9 +19,37 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            showTodos();
+            break;
         default:
             console.log('wrong command');
             break;
+    }
+}
+
+function showTodos() {
+    const todos = [];
+    files.forEach(file => {
+        const lines = file.content.split('\n');
+        lines.forEach((line, index) => {
+            if (line.trim().startsWith('// TODO')) {
+                todos.push({
+                    file: file.path,
+                    lineNumber: index + 1,
+                    comment: line.trim()
+                });
+            }
+        });
+    });
+
+    if (todos.length === 0) {
+        console.log('No TODOs found.');
+    } else {
+        console.log('TODOs found:');
+        todos.forEach(todo => {
+            console.log(`File: ${todo.file}, Line: ${todo.lineNumber}, Comment: ${todo.comment}`);
+        });
     }
 }
 
